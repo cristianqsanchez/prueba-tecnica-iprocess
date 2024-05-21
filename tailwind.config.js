f18f2fd4be1459d1d0de644121201b67bad5dd27
cristianqsanchez <cristianquintero316@gmail.com>
2024-05-20 19:17:09 -0500

    add system design colors in tailwind scope

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: 'var(--primary)',
+        'primary-light': 'var(--primary-light)',
+        succes: 'var(--success)',
+        neutral: 'var(--neutral)'
+      }
+    }
   },
   plugins: []
 }
