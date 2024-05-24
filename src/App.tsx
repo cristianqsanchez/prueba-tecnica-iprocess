25fd0b5bab877cd9e2aa40cc142fb16a1e58c44c
cristianqsanchez <cristianquintero316@gmail.com>
2024-05-23 20:23:31 -0500

    feat: add payment list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { DEFAULT_SETTINGS } from '@/consts'
 import { PaymentsSettingsProvider } from '@/context/payments-settings'
+import { PaymentsProvider } from '@/context/payments'
 import { Header } from '@/components/header'
+import { Pagos } from '@/components/pagos'
+import { Toaster } from 'sonner'
 
 function App () {
   return (
     <>
       <PaymentsSettingsProvider {...DEFAULT_SETTINGS}>
         <Header />
+        <PaymentsProvider>
+          <Pagos />
+        </PaymentsProvider>
       </PaymentsSettingsProvider>
+      <Toaster position='top-right' />
     </>
   )
 }
