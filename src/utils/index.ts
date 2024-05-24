8fdbf2c74c7fa42eef1bd55695b833f4bc4b083e
cristianqsanchez <cristianquintero316@gmail.com>
2024-05-23 21:31:11 -0500

    fix: add current date now

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,9 @@
-export const formatInputDate = (date: Date) => date.toISOString().split('T').at(0) as string
+export function formatInputDate (date: Date) {
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+
+  return `${year}-${month}-${day}`
+}
 
 export const randomID = () => crypto.randomUUID() as string
