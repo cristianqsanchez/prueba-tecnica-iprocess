bb3e1866c0adbc36166430eec958c9e29abfa992
cristianqsanchez <cristianquintero316@gmail.com>
2024-05-23 23:02:51 -0500

    fix: add rounded numbers

--- src/components/payment-amount.tsx
+++ src/components/payment-amount.tsx
@@ -2,8 +2,8 @@ export function PaymentAmount ({ amount, currency, percentage }:
   { amount: number, currency: string, percentage: number }) {
   return (
     <div className="flex gap-2">
-      <strong>{amount} {currency}</strong>
-      <span>{percentage} %</span>
+      <strong>{amount.toFixed(1)} {currency}</strong>
+      <span>{percentage.toFixed()} %</span>
     </div>
   )
 }
